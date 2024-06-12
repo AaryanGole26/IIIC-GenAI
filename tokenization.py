78d124856047f248580dadf0fe26eb191ef923f4
AARYAN  GOLE <72779304+AaryanGole26@users.noreply.github.com>
2024-06-12 11:21:44 +0530

    Add files via upload

---
+++ tokenization.py
@@ -0,0 +1,12 @@
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+
+text = 'This is Generative AI workshop'
+
+sentences = sent_tokenize (text)
+print(sentences)
+
+words = word_tokenize (text)
+
+for i in words:
+    print (i)
